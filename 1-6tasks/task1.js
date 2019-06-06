b71bdff9a5f105f9bdf623cfa12c9d7ed9a2c6a5
DigitalG <Thesever99@gmail.com>
2019-06-07 00:10:43 +0300

    'fix'

--- 1-6tasks/task1.js
+++ 1-6tasks/task1.js
@@ -6,15 +6,19 @@ function StringToArray (string) {
   let result = []
   let start = 0
   for (let i = 0; i <= string.length; i++) {
-    if (string[i] === ' ') {
+    if (string[i] === ' ' && start !==-1) {
       result.push(string.slice(start, i))
-      start = i + 1
+      start = -1
     }
+
+    if(string[i+1] !== ' ' && start === -1){
+        start = i + 1
+      }
   }
   result.push(string.slice(start, string.length))
   return result
 }
 
-var MyString = 'one two three four'
+var MyString = 'one two   three   four'
 console.log(MyString)
 console.log(StringToArray(MyString))
